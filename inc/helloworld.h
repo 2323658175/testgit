968e1ce1fc84dcabdce731a2f392e44d5e47fa36
frosted <2323658175@qq.com>
2022-07-14 17:21:22 +0800

    initial project version

---
+++ inc/helloworld.h
@@ -0,0 +1,6 @@
+#ifndef __HELLOWORLD
+#define __HELLOWORLD
+#include<stdio.h>
+void printhello();
+int main();
+#endif
---
+++ src/helloworld.c
@@ -0,0 +1,8 @@
+#include "helloworld.h"
+int main(){
+    printhello();
+    return 0;
+}
+void printhello(){
+    printf("hello world!");
+}
